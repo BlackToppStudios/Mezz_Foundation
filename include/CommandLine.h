6de2f720955f315b464becc0eabab54b75714575
Sqeaky <sqeaky@blacktoppstudios.com>
2019-03-03 02:36:06 -0600

    Added simple class and functions for handling command like args

---
+++ include/CommandLine.h
@@ -0,0 +1,242 @@
+// © Copyright 2010 - 2018 BlackTopp Studios Inc.
+/* This file is part of The Mezzanine Engine.
+
+    The Mezzanine Engine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Mezzanine Engine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with The Mezzanine Engine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/* The original authors have included a copy of the license specified above in the
+   'Docs' folder. See 'gpl.txt'
+*/
+/* We welcome the use of the Mezzanine engine to anyone, including companies who wish to
+   Build professional software and charge for their product.
+
+   However there are some practical restrictions, so if your project involves
+   any of the following you should contact us and we will try to work something
+   out:
+    - DRM or Copy Protection of any kind(except Copyrights)
+    - Software Patents You Do Not Wish to Freely License
+    - Any Kind of Linking to Non-GPL licensed Works
+    - Are Currently In Violation of Another Copyright Holder's GPL License
+    - If You want to change our code and not add a few hundred MB of stuff to
+        your distribution
+
+   These and other limitations could cause serious legal problems if you ignore
+   them, so it is best to simply contact us or the Free Software Foundation, if
+   you have any questions.
+
+   Joseph Toppi - toppij@gmail.com
+   John Blackwood - makoenergy02@gmail.com
+*/
+#ifndef Mezz_Foundation_CommandLine_h
+#define Mezz_Foundation_CommandLine_h
+
+/// @file
+/// @brief a small set of tools for parsing command line arguments.
+
+#ifndef SWIG
+    #include "DataTypes.h"
+    #include "FlatMap.h"
+#endif
+
+namespace Mezzanine
+{
+
+/// @brief Represent an ordered collection of command line arguments as strings.
+using ArgVector = std::vector<Mezzanine::String>;
+
+/// @brief A mapping of command line switches/flags onto possible argument vectors for those flags.
+using ArgMap = Mezzanine::FlatMap<Mezzanine::String, ArgVector>;
+
+/// @brief This token is used to prefix arguments/flags on the command line.
+const char ArgToken = '-';
+
+/// @brief Convey nain args into a vector with no semantic changes
+/// @param ArgCount The count of arguments as given to main.
+/// @param Arguments The "Argument Vector" as given to main.
+/// @return An ArgVector with a 1 to 1 conversion of the inputs without opinions in parsing or tokenizing.
+ArgVector MEZZ_LIB VectorizeArgs(int ArgCount,  char** Arguments);
+
+/// @brief An opinionated way to split grouped single letter args into multiple short arguments
+/// @param DirtyArg A single arg prepended with '-' that should be split.
+/// @return An array of single character args, "-zxf" would yield {"-z", "-x", "-f"}.
+ArgVector MEZZ_LIB TokenizeShortArg(const Mezzanine::String& DirtyArg);
+
+/// @brief Break a single token into numerous tokens if they are all short args.
+/// @param DirtyArg A group of short args like "-a" or "-rf"
+/// @return An ArgVector with short tokens for each long token like ["-a"] or ["-r", "-f"]
+ArgVector MEZZ_LIB TokenizeSingleArg(const Mezzanine::String& DirtyArg);
+
+/// @brief Break any amount of argument tokens into smaller argument tokens
+/// @param DirtyArgs A vector of arguments that might include some arguments stuck together.
+/// @return A vector containing all the tokens the passed token vector contained, but all completely individual.
+ArgVector MEZZ_LIB TokenizeArgVector(const ArgVector& DirtyArgs);
+
+/// @brief Map Arguments onto their parameters. Combine flags that accept params with those params.
+/// @param DirtyArgs A vector of fully tokenized args to
+/// @return A Map of flags/switchs starting with "-" onto a vector of things without these prefixes.
+ArgMap MEZZ_LIB MapArgumentParameters(const ArgVector& DirtyArgs);
+
+/// @brief A collection of arguments and possible following flags.
+/// @details This is a simple container for storing for command line arguments after parsing them in a mildly
+/// opinionated but common way.
+/// @n @n
+/// This accepts the Argument Count (commonly argc) and Argument Vector/Values(argv) in the constructor as they are
+/// passed into main.
+/// @code{.cpp}
+/// int main(int ArgC, char** ArgV)
+/// {
+///      const CommandLineArguments Parsed(ArgC, ArgV);
+/// @endcode
+/// This tries to look a little like typical GNU command line arguments. This handles "long"
+/// argument that start with "--" like "--all" and this handles "short" arguments that start with "-" like "-a and
+/// combined short forms like "-rf".
+/// @n @n
+/// This also handles arguments without a "-" prefix by presuming they are parameters for previous arguments. Many
+/// arguments need additional information to specify some behavior. Consider a potential zip command, it might accept a
+/// "--file" flag to indicate the name of the zip file to work with.
+/// @n @n
+/// This makes no further guarantees about preserving the order arguments are passed in. For example "-wtf" is the same
+/// as "-tfw" are treated the same.
+/// @n @n
+/// This converts all arguments to keys in a map with a value of a vector of their parameters. Consider this command:
+/// @n @n
+///     foo.exe -s
+/// @n @n
+/// This would store "foo.exe" in the data member ExecutableCommand and turns the arg into a dictionary with one key,
+/// "-s", and that key would have an empty vector for a value. As an std::initializer_list it might look like:
+/// @n @n
+///     { {"-s", {} }
+/// @n @n
+/// All of these examples have the same executable command and but would make slightly data structures and are
+/// illustrated as possible initializer lists:
+/// @n @n
+///
+/// Short arguments are broken up, and long arguments are not. Short arguments have one "-", long have two:
+/// @n @n
+///    foo.exe -sf --long
+/// @n @n
+///     {
+///         { "--long", {} },
+///         { "-s",     {} },
+///         { "-w",     {} }
+///     }
+/// @n @n
+/// @code{.cpp}
+/// int main(int ArgC, char** ArgV)
+/// {
+///      const CommandLineArguments Parsed(ArgC, ArgV);
+///      std::cout
+///         << Parsed.ExecutableCommand // foo.exe
+///         << Parsed.Arguments.at("--long").size() // 0
+///         << Parsed.Arguments.contains("-s"); // true
+/// @endcode
+///
+/// Items following arguments with not "-" are treating as parameters to the argument they follow.
+/// @n @n
+///    foo.exe -s --long trailing -w trailing_short
+/// @n @n
+///     {
+///         { "-s",     {} },
+///         { "--long", {"trailing"} },
+///         { "-w",     {"trailing_short"} }
+///     }
+/// @n @n
+/// @code{.cpp}
+/// int main(int ArgC, char** ArgV)
+/// {
+///      const CommandLineArguments Parsed(ArgC, ArgV);
+///      std::cout
+///         << Parsed.Arguments.at("--long").at(0) // trailing
+///         << Parsed.Arguments.at("-w").at(0); // trailing_short
+/// @endcode
+///
+/// Multiple arguments of the same kind are mapped onto the same key,
+/// @n @n
+///    foo.exe -wtfbbq
+/// @n @n
+///     {
+///         { "-w",     {} },
+///         { "-t",     {} },
+///         { "-f",     {} },
+///         { "-b",     {} },
+///         { "-q",     {} }
+///     }
+/// @n @n
+///
+/// If you pass multiple arguments with "-" they keeping appending to that vector in the resulting map even if that
+/// argument is passed multiple times on the command line:
+/// @n @n
+///     foo.exe -a arg1 -pa arg2 arg3 -x arg4
+/// @n @n
+///     {
+///         { "-a", {"arg1", "arg2"}, "arg3"} },
+///         { "-p", {} },
+///         { "-x", {"arg4"} }
+///     }
+/// @n @n
+/// @code{.cpp}
+/// int main(int ArgC, char** ArgV)
+/// {
+///      const CommandLineArguments Parsed(ArgC, ArgV);
+///      std::cout
+///         << Parsed.Arguments.at("-a").at(0)  // arg1
+///         << Parsed.Arguments.at("-a").at(1)  // arg2
+///         << Parsed.Arguments.at("-a").at(2)  // arg3
+///         << Parsed.Arguments.at("-x").at(0)  // arg4
+/// @endcode
+///
+/// All of together might look like:
+/// @n @n
+///    foo.exe -s --long trailing -wtfbbq suace --long arg2
+/// @n @n
+///     {
+///         { "-s",     {} },
+///         { "--long", {"trailing", "arg2"} },
+///         { "-w",     {} },
+///         { "-t",     {} },
+///         { "-f",     {} },
+///         { "-b",     {} },
+///         { "-q",     {"sauce"} }
+///     }
+class MEZZ_LIB CommandLineArguments
+{
+public:
+    /// @brief The command used to invokd the executable.
+    const Mezzanine::String ExecutableCommand;
+
+    /// @brief The arguments parsed with the command GNU semantics included.
+    const Mezzanine::ArgMap Arguments;
+
+
+    /// @brief Constructor From main params. This will fill all values on an instance of this class.
+    /// @param ArgCount The count of args from main.
+    /// @param ArgValues The Argument Vector or Argument Values from Main.
+    CommandLineArguments(int ArgCount,  char** ArgValues);
+
+private:
+    /// @brief Get the name of the executable launched here.
+    /// @param ArgCount The count of args from main.
+    /// @param ArgValues The Argument Vector or Argument Values from Main.
+    /// @return The Executable or an empty string.
+    Mezzanine::String ExecutableFromArgV(int ArgCount,  char** ArgValues) const;
+
+    /// @brief Parse all the args except for the executable name
+    /// @param ArgCount The count of args from main.
+    /// @param ArgValues The Argument Vector or Argument Values from Main.
+    /// @return A mapping of params as MapArgumentParameters would return.
+    ArgMap ArgsFromArgV(int ArgCount,  char** ArgValues) const;
+};
+
+} //Mezzanine
+
+#endif // Include guard
---
+++ test/CommandLineTests.h
@@ -0,0 +1,218 @@
+// © Copyright 2010 - 2019 BlackTopp Studios Inc.
+/* This file is part of The Mezzanine Engine.
+
+    The Mezzanine Engine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Mezzanine Engine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with The Mezzanine Engine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/* The original authors have included a copy of the license specified above in the
+   'Docs' folder. See 'gpl.txt'
+*/
+/* We welcome the use of the Mezzanine engine to anyone, including companies who wish to
+   Build professional software and charge for their product.
+
+   However there are some practical restrictions, so if your project involves
+   any of the following you should contact us and we will try to work something
+   out:
+    - DRM or Copy Protection of any kind(except Copyrights)
+    - Software Patents You Do Not Wish to Freely License
+    - Any Kind of Linking to Non-GPL licensed Works
+    - Are Currently In Violation of Another Copyright Holder's GPL License
+    - If You want to change our code and not add a few hundred MB of stuff to
+        your distribution
+
+   These and other limitations could cause serious legal problems if you ignore
+   them, so it is best to simply contact us or the Free Software Foundation, if
+   you have any questions.
+
+   Joseph Toppi - toppij@gmail.com
+   John Blackwood - makoenergy02@gmail.com
+*/
+#ifndef Mezz_Foundation_CommandLineTests_h
+#define Mezz_Foundation_CommandLineTests_h
+
+/// @file
+/// @brief
+
+#include "MezzTest.h"
+
+#include "CommandLine.h"
+
+SAVE_WARNING_STATE
+
+DEFAULT_TEST_GROUP(CommandLineTests, CommandLine)
+{
+    using Mezzanine::String;
+
+    using Mezzanine::ArgVector;
+    using Mezzanine::VectorizeArgs;
+    using Mezzanine::TokenizeShortArg;
+    using Mezzanine::TokenizeSingleArg;
+    using Mezzanine::TokenizeArgVector;
+    using Mezzanine::ArgMap;
+    using Mezzanine::MapArgumentParameters;
+    using Mezzanine::CommandLineArguments;
+
+
+    {
+        const char* ArgValueArray[] = {
+            "foo.exe",
+            "-s",
+            "--long",
+            "trailing",
+            "-wtfbbq",
+            0
+        };
+        char** ArgV = const_cast<char**>(ArgValueArray);
+        const int ArgC = 5;
+
+        ArgVector VectorizedArgs(VectorizeArgs(ArgC, ArgV));
+        TEST_EQUAL("VectorizeArgs - Count",     size_t(5),          VectorizedArgs.size());
+        TEST_EQUAL("VectorizeArgs - 0",         String("foo.exe"),  VectorizedArgs.at(0));
+        TEST_EQUAL("VectorizeArgs - 1",         String("-s"),       VectorizedArgs.at(1));
+        TEST_EQUAL("VectorizeArgs - 2",         String("--long"),   VectorizedArgs.at(2));
+        TEST_EQUAL("VectorizeArgs - 3",         String("trailing"), VectorizedArgs.at(3));
+        TEST_EQUAL("VectorizeArgs - 4",         String("-wtfbbq"),  VectorizedArgs.at(4));
+    }
+
+    // Complex Short Tokenization
+    {
+        ArgVector ShortTokensizedArgs = TokenizeShortArg("-wtfbbq");
+        TEST_EQUAL("TokenizeShortArg - Complex - Count",size_t(6),      ShortTokensizedArgs.size());
+        TEST_EQUAL("TokenizeShortArg - Complex - 0",    String("-w"),   ShortTokensizedArgs.at(0));
+        TEST_EQUAL("TokenizeShortArg - Complex - 1",    String("-t"),   ShortTokensizedArgs.at(1));
+        TEST_EQUAL("TokenizeShortArg - Complex - 2",    String("-f"),   ShortTokensizedArgs.at(2));
+        TEST_EQUAL("TokenizeShortArg - Complex - 3",    String("-b"),   ShortTokensizedArgs.at(3));
+        TEST_EQUAL("TokenizeShortArg - Complex - 4",    String("-b"),   ShortTokensizedArgs.at(4));
+        TEST_EQUAL("TokenizeShortArg - Complex - 5",    String("-q"),   ShortTokensizedArgs.at(5));
+    }
+
+    // Simple short tokenization
+    {
+        ArgVector ShortTokensizedArgs = TokenizeShortArg("-f");
+        TEST_EQUAL("TokenizeShortArg - Simple - Count", size_t(1),      ShortTokensizedArgs.size());
+        TEST_EQUAL("TokenizeShortArg - Simple - 0",     String("-f"),   ShortTokensizedArgs.at(0));
+    }
+
+    // Complex Single Short Tokenization
+    {
+        ArgVector TokenizedArgs = TokenizeSingleArg("-wtfbbq");
+        TEST_EQUAL("TokenizeSingleArg - Short - Count", size_t(6),      TokenizedArgs.size());
+        TEST_EQUAL("TokenizeSingleArg - Short - 0",     String("-w"),   TokenizedArgs.at(0));
+        TEST_EQUAL("TokenizeSingleArg - Short - 1",     String("-t"),   TokenizedArgs.at(1));
+        TEST_EQUAL("TokenizeSingleArg - Short - 2",     String("-f"),   TokenizedArgs.at(2));
+        TEST_EQUAL("TokenizeSingleArg - Short - 3",     String("-b"),   TokenizedArgs.at(3));
+        TEST_EQUAL("TokenizeSingleArg - Short - 4",     String("-b"),   TokenizedArgs.at(4));
+        TEST_EQUAL("TokenizeSingleArg - Short - 5",     String("-q"),   TokenizedArgs.at(5));
+    }
+
+    // Complex Single Long Tokenization
+    {
+        ArgVector TokensizedArgs = TokenizeSingleArg("--wtfbbq");
+        TEST_EQUAL("TokenizeSingleArg - Long - Count", size_t(1),           TokensizedArgs.size());
+        TEST_EQUAL("TokenizeSingleArg - Long - 0",     String("--wtfbbq"),  TokensizedArgs.at(0));
+    }
+
+    // Complex Multiple Tokenization
+    {
+        ArgVector TokenizedArgs = TokenizeArgVector({"-lmfao", "--lmfao"});
+        TEST_EQUAL("TokenizeArgVector - Count", size_t(6),          TokenizedArgs.size());
+        TEST_EQUAL("TokenizeArgVector - 0",     String("-l"),       TokenizedArgs.at(0));
+        TEST_EQUAL("TokenizeArgVector - 1",     String("-m"),       TokenizedArgs.at(1));
+        TEST_EQUAL("TokenizeArgVector - 2",     String("-f"),       TokenizedArgs.at(2));
+        TEST_EQUAL("TokenizeArgVector - 3",     String("-a"),       TokenizedArgs.at(3));
+        TEST_EQUAL("TokenizeArgVector - 4",     String("-o"),       TokenizedArgs.at(4));
+        TEST_EQUAL("TokenizeArgVector - 5",     String("--lmfao"),  TokenizedArgs.at(5));
+    }
+
+    {
+        ArgVector TokenizedArgs({"-z", "-x", "-f", "input.tar", "-v"});
+        ArgMap MappedArgs = MapArgumentParameters(TokenizedArgs);
+        TEST_EQUAL("MapArgumentParameters - Count",         size_t(4),              MappedArgs.size());
+        TEST_EQUAL("MapArgumentParameters - z 0",           size_t(0),              MappedArgs.at("-z").size());
+        TEST_EQUAL("MapArgumentParameters - x 0",           size_t(0),              MappedArgs.at("-x").size());
+        TEST_EQUAL("MapArgumentParameters - f 1",           size_t(1),              MappedArgs.at("-f").size());
+        TEST_EQUAL("MapArgumentParameters - v 0",           size_t(0),              MappedArgs.at("-v").size());
+        TEST_EQUAL("MapArgumentParameters - file false",    false,                  MappedArgs.contains("input.tar"));
+        TEST_EQUAL("MapArgumentParameters - f with file",   String("input.tar"),    MappedArgs.at("-f").at(0));
+
+        ArgVector DefaultArgs({"input.tar"});
+        ArgMap MappedDefaultArgs = MapArgumentParameters(DefaultArgs);
+        TEST_EQUAL("MapArgsDefault - Count",                size_t(1),              MappedDefaultArgs.size());
+        TEST_EQUAL("MapArgsDefault - Default --",           true,                   MappedDefaultArgs.contains("--"));
+        TEST_EQUAL("MapArgsDefault - filename exist",       String("input.tar"),    MappedDefaultArgs.at("--").at(0));
+
+        ArgVector EmptyArgs;
+        ArgMap MappedEmptyArgs = MapArgumentParameters(EmptyArgs);
+        TEST_EQUAL("MapArgsEmpty - Count",         size_t(0),   MappedEmptyArgs.size());
+    }
+
+    // The final classes for use by end users.
+    {
+        const char* ArgValueArray[] = {
+            "foo.exe",
+            "-s",
+            "--long",
+            "trailing",
+            "-wtfbbq",
+            0
+        };
+        char** ArgV = const_cast<char**>(ArgValueArray);
+        const int ArgC = 5;
+
+        const CommandLineArguments Parsed(ArgC, ArgV);
+
+        TEST_EQUAL("CommandLineArguments - ExecutableCommand",      String("foo.exe"),   Parsed.ExecutableCommand);
+        TEST_EQUAL("CommandLineArguments - ArgCount",   size_t(7),              Parsed.Arguments.size());
+        TEST_EQUAL("CommandLineArguments - s",          size_t(0),              Parsed.Arguments.at("-s").size());
+        TEST_EQUAL("CommandLineArguments - long",       size_t(1),              Parsed.Arguments.at("--long").size());
+        TEST_EQUAL("CommandLineArguments - long arg",   String("trailing"),     Parsed.Arguments.at("--long").at(0));
+        TEST_EQUAL("CommandLineArguments - w",          size_t(0),              Parsed.Arguments.at("-w").size());
+        TEST_EQUAL("CommandLineArguments - t",          size_t(0),              Parsed.Arguments.at("-t").size());
+        TEST_EQUAL("CommandLineArguments - f",          size_t(0),              Parsed.Arguments.at("-f").size());
+        TEST_EQUAL("CommandLineArguments - b",          size_t(0),              Parsed.Arguments.at("-b").size());
+        TEST_EQUAL("CommandLineArguments - q",          size_t(0),              Parsed.Arguments.at("-q").size());
+    }
+
+    // The final classes for use by end users.
+    {
+        const char* ArgValueArray[] = {
+            "foo.exe",
+            "-a",
+            "arg1",
+            "-pa",
+            "arg2",
+            "arg3",
+            "-x",
+            "arg4",
+            0
+        };
+        char** ArgV = const_cast<char**>(ArgValueArray);
+        const int ArgC = 8;
+
+        const CommandLineArguments Parsed(ArgC, ArgV);
+
+        TEST_EQUAL("multipleParams - ExecutableCommand",    String("foo.exe"),   Parsed.ExecutableCommand);
+        TEST_EQUAL("multipleParams - ArgCount",     size_t(3),          Parsed.Arguments.size());
+        TEST_EQUAL("multipleParams - x",            size_t(1),          Parsed.Arguments.at("-x").size());
+        TEST_EQUAL("multipleParams - p",            size_t(0),          Parsed.Arguments.at("-p").size());
+        TEST_EQUAL("multipleParams - z",            size_t(3),          Parsed.Arguments.at("-a").size());
+        TEST_EQUAL("multipleParams - x1",           String("arg4"),     Parsed.Arguments.at("-x").at(0));
+        TEST_EQUAL("multipleParams - a1",           String("arg1"),     Parsed.Arguments.at("-a").at(0));
+        TEST_EQUAL("multipleParams - a2",           String("arg2"),     Parsed.Arguments.at("-a").at(1));
+        TEST_EQUAL("multipleParams - a3",           String("arg3"),     Parsed.Arguments.at("-a").at(2));
+    }
+
+}
+RESTORE_WARNING_STATE
+
+#endif
